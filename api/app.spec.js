1264c84ace015e589506888684fc97234ce136ea
Simon Davey <simonr.davey@gmail.com>
2019-07-23 15:05:27 +0100

    Adds first test of produce-report endpoint.

---
+++ api/app.spec.js
@@ -0,0 +1,9 @@
+const app = require('./app');
+const request = require('supertest')(app);
+const { expect } = require('chai');
+
+describe('/api/produce-report', () => {
+    it('POST takes an object with a URL', () => {
+        return request.post('/api/produce-report').send({ url: 'https://broken-links-api.herokuapp.com/'}).expect(200);
+    })
+})
